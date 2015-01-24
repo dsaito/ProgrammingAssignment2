be458ebf4e16d5e3e57c7dcdf9aadc3c23db1a06
System Administrator <root@MacBook-Air-de-Daniel.local>
2015-01-24 12:09:24 -0400

    - This is the final version of the cachematrix.R file, as a requisite for the Programming Assignment 2 of the Coursera R Programming Course. - Committer: dsaito - Changes to be committed:   modified:   cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions should work together to: 
+## 1) create a cacheable matrix (makeCacheMatrix)
+## 2) invert the matrix itself (cacheSolve)
 
-## Write a short comment describing this function
+## makeCacheMatrix is a creator function used to obtain a matrix that is cacheable for subsequent
+## analysis (inversion by the cacheSolve function, that is)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <<- NULL
+    getmatrix <- function() x
+    setinverse <- function(z) inv <<- z
+    getinverse <- function() inv
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is a function that calculates the inverse of the matrix previously
+## created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- getinverse()
+    if(!is.null(inv)) {
+        message("Cached inverse matrix:")
+        print(inv)
+    } else {
+        data <- l$get()
+        inv <- solve(data, ...)
+        setinverse(inv)
+        message("Computed inverse matrix:")
+        print(inv)
+    }
 }
